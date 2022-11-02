7a16939fd236dface6bd2ae6bc75f08afd350c26
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-11-02 17:02:02 -0600

    quiz answers and timer now work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,115 @@
 // declare variables
 var start = document.querySelector(".start");
+var timer = document.getElementById("timer");
+var timeLeft = 120;
 
-// Should this go below var question ?
-start.addEventListener("click", function(){
-    console.log(start);
-    // Upon pressing start button, first question appears --- is everything that follows inside one giant function?
-});
+var qIndex = 0
+var questionEl = document.querySelector("#question")
+var ans1 = document.querySelector("#ans1")
+var ans2 = document.querySelector("#ans2")
+var ans3 = document.querySelector("#ans3")
+var ans4 = document.querySelector("#ans4")
 
-// questions var = [array]
-var question = [{
+var questions = [{
     // first question
     title: "This is question 1",
-    choices: ["a", "b", "c"],
+    choices: ["a", "b", "c", "d"],
     answer: "a",},
 
     // second question
    { title: "This is question 2",
-    choices: ["a","b", "c"],
-    answer: "c"},
+    choices: ["a1","b1", "c1", "d1"],
+    answer: "c1"},
 
     // third question
     {title: "This is question 3",
-    choices: ["a","b", "c"],
-    answer: "c"},
+    choices: ["a2","b2", "c2", "d2"],
+    answer: "c2"},
 
     // fourth question
 
     // fifth question
 ];
 
+
+
+
+
+// Should this go below var question ?
+start.addEventListener("click", function(){
+    console.log(start);
+    var questionArea = document.querySelector(".question-el");
+    questionArea.style = "display: block;";
+    var instructions = document.querySelector(".open-page");
+    instructions.style = "display: none;";
+    // Upon pressing start button, first question appears --- is everything that follows inside one giant function?
+    //timer - needs to start when user presses start
+    quiz();
+    countdown()
+});
+
+
+function quiz(){
+    questionEl.textContent = questions[qIndex].title
+    ans1.textContent = questions[qIndex].choices[0];
+    ans2.textContent = questions[qIndex].choices[1];
+    ans3.textContent = questions[qIndex].choices[2];
+    ans4.textContent = questions[qIndex].choices[3];
+    if (qIndex == questions.length-1) {
+        return;
+    } else {
+        qIndex++;
+    }
 // Upon pressing a choice for a question, it will return whether user is correct or not
-// Upon pressing next, the next question appears
-for (var i=0; i < question.length; i++){
 }
 
+function checkAns(){
+    if (this.textContent!==questions[qIndex].answer) {
+        timeLeft
+    } 
+}
 
-var timer = document.getElementById("timer");
+ans1.addEventListener("click", function(e) { 
+    if (e.target.textContent!==questions[qIndex].answer) {
+        timeLeft -= 10
+        console.log("test")
+    } 
+    quiz()
+})
+ans2.addEventListener("click", function(e) {
+    if (e.target.textContent!==questions[qIndex].answer) {
+        timeLeft -= 10
+        console.log("test")
+    } 
+    quiz()
+})
+ans3.addEventListener("click", function(e) {
+    if (e.target.textContent!==questions[qIndex].answer) {
+        timeLeft -= 10
+        console.log("test")
+    } 
+    quiz()
+})
+ans4.addEventListener("click", function(e) {
+    if (e.target.textContent!==questions[qIndex].answer) {
+        timeLeft -= 10
+        console.log("test")
+    } 
+    quiz()
+})
 
+// timer var at line 3
 function countdown() {
-    var timeLeft = 120;
-
     setInterval(function () {
         if (timeLeft > 0) {
-            timer.textcontent = timeLeft;
+            timer.textContent = timeLeft;
+            // console.log(timeLeft)
             timeLeft--;
         } else {
             timer.textContent = 0;
         }
     }, 1000);
 }
-//timer - needs to start when user presses start
 // time being subtracted when user gets answer wrong
 //If timeLeft = 0 then it sends user to "you lose" screen
 // End screen with score and post your results form, score = amount of time left
