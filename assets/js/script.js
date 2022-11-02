71497d66ea6981e08a733a2b08d4d261e4621d0c
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-11-02 10:43:21 -0600

    Started JS timer, does not work yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,55 @@
 // declare variables
+var start = document.querySelector(".start");
 
-// Upon pressing start button, first question appears
+// Should this go below var question ?
+start.addEventListener("click", function(){
+    console.log(start);
+    // Upon pressing start button, first question appears --- is everything that follows inside one giant function?
+});
 
 // questions var = [array]
+var question = [{
+    // first question
+    title: "This is question 1",
+    choices: ["a", "b", "c"],
+    answer: "a",},
 
-// first question
+    // second question
+   { title: "This is question 2",
+    choices: ["a","b", "c"],
+    answer: "c"},
 
+    // third question
+    {title: "This is question 3",
+    choices: ["a","b", "c"],
+    answer: "c"},
+
+    // fourth question
+
+    // fifth question
+];
+
+// Upon pressing a choice for a question, it will return whether user is correct or not
 // Upon pressing next, the next question appears
+for (var i=0; i < question.length; i++){
+}
 
-// second question
 
-// third question
+var timer = document.getElementById("timer");
 
-// fourth question
+function countdown() {
+    var timeLeft = 120;
 
-// fifth question
-
-// End screen with score and post your results form
+    setInterval(function () {
+        if (timeLeft > 0) {
+            timer.textcontent = timeLeft;
+            timeLeft--;
+        } else {
+            timer.textContent = 0;
+        }
+    }, 1000);
+}
+//timer - needs to start when user presses start
+// time being subtracted when user gets answer wrong
+//If timeLeft = 0 then it sends user to "you lose" screen
+// End screen with score and post your results form, score = amount of time left
